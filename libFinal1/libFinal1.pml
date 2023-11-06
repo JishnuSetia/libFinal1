12d7bf8f2a438fd816f5ac430bb41e1fc2817f6d
Jishnu Setia <85150122+JishnuSetia@users.noreply.github.com>
2023-11-06 10:12:00 +0400

    Add files via upload

---
+++ libFinal1/libFinal1.pml
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="libFinal1" format_version="5">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="intro" src="intro/intro.dlg" />
+        <Dialog name="close" src="close/close.dlg" />
+        <Dialog name="fiction" src="fiction/fiction.dlg" />
+        <Dialog name="nonfiction" src="nonfiction/nonfiction.dlg" />
+        <Dialog name="choose" src="choose/choose.dlg" />
+        <Dialog name="primary" src="primary/primary.dlg" />
+        <Dialog name="high" src="high/high.dlg" />
+        <Dialog name="p1" src="p1/p1.dlg" />
+        <Dialog name="ExampleDialog" src="behavior_1/ExampleDialog/ExampleDialog.dlg" />
+        <Dialog name="p2" src="p2/p2.dlg" />
+        <Dialog name="p3" src="p3/p3.dlg" />
+        <Dialog name="m1" src="m1/m1.dlg" />
+        <Dialog name="m2" src="m2/m2.dlg" />
+        <Dialog name="m3" src="m3/m3.dlg" />
+        <Dialog name="h1" src="h1/h1.dlg" />
+        <Dialog name="h2" src="h2/h2.dlg" />
+        <Dialog name="h3" src="h3/h3.dlg" />
+        <Dialog name="general" src="general/general.dlg" />
+        <Dialog name="first" src="first/first.dlg" />
+        <Dialog name="second" src="second/second.dlg" />
+        <Dialog name="third" src="third/third.dlg" />
+        <Dialog name="four" src="four/four.dlg" />
+        <Dialog name="first1" src="first1/first1.dlg" />
+        <Dialog name="second1" src="second1/second1.dlg" />
+        <Dialog name="third1" src="third1/third1.dlg" />
+        <Dialog name="four1" src="four1/four1.dlg" />
+        <Dialog name="first2" src="first2/first2.dlg" />
+        <Dialog name="second2" src="second2/second2.dlg" />
+        <Dialog name="third2" src="third2/third2.dlg" />
+        <Dialog name="four2" src="four2/four2.dlg" />
+        <Dialog name="test" src="test/test.dlg" />
+        <Dialog name="morereco" src="morereco/morereco.dlg" />
+    </Dialogs>
+    <Resources>
+        <File name="Library-1" src="html/4.jpg" />
+        <File name="1050" src="html/1050.png" />
+        <File name="450" src="html/450.jpg" />
+        <File name="750" src="html/750.png" />
+        <File name="index" src="html/index.html" />
+        <File name="script" src="html/script.js" />
+        <File name="typed.min" src="html/typed.min.js" />
+    </Resources>
+    <Topics>
+        <Topic name="intro_enu" src="intro/intro_enu.top" topicName="intro" language="en_US" nuance="enu" />
+        <Topic name="close_enu" src="close/close_enu.top" topicName="close" language="en_US" nuance="enu" />
+        <Topic name="fiction_enu" src="fiction/fiction_enu.top" topicName="fiction" language="en_US" nuance="enu" />
+        <Topic name="nonfiction_enu" src="nonfiction/nonfiction_enu.top" topicName="nonfiction" language="en_US" nuance="enu" />
+        <Topic name="choose_enu" src="choose/choose_enu.top" topicName="choose" language="en_US" nuance="enu" />
+        <Topic name="primary_enu" src="primary/primary_enu.top" topicName="primary" language="en_US" nuance="enu" />
+        <Topic name="high_enu" src="high/high_enu.top" topicName="high" language="en_US" nuance="enu" />
+        <Topic name="p1_enu" src="p1/p1_enu.top" topicName="p1" language="en_US" nuance="enu" />
+        <Topic name="ExampleDialog_enu" src="behavior_1/ExampleDialog/ExampleDialog_enu.top" topicName="ExampleDialog" language="en_US" nuance="enu" />
+        <Topic name="p2_enu" src="p2/p2_enu.top" topicName="p2" language="en_US" nuance="enu" />
+        <Topic name="p3_enu" src="p3/p3_enu.top" topicName="p3" language="en_US" nuance="enu" />
+        <Topic name="m1_enu" src="m1/m1_enu.top" topicName="m1" language="en_US" nuance="enu" />
+        <Topic name="m2_enu" src="m2/m2_enu.top" topicName="m2" language="en_US" nuance="enu" />
+        <Topic name="m3_enu" src="m3/m3_enu.top" topicName="m3" language="en_US" nuance="enu" />
+        <Topic name="h1_enu" src="h1/h1_enu.top" topicName="h1" language="en_US" nuance="enu" />
+        <Topic name="h2_enu" src="h2/h2_enu.top" topicName="h2" language="en_US" nuance="enu" />
+        <Topic name="h3_enu" src="h3/h3_enu.top" topicName="h3" language="en_US" nuance="enu" />
+        <Topic name="general_enu" src="general/general_enu.top" topicName="general" language="en_US" nuance="enu" />
+        <Topic name="first_enu" src="first/first_enu.top" topicName="first" language="en_US" nuance="enu" />
+        <Topic name="second_enu" src="second/second_enu.top" topicName="second" language="en_US" nuance="enu" />
+        <Topic name="third_enu" src="third/third_enu.top" topicName="third" language="en_US" nuance="enu" />
+        <Topic name="four_enu" src="four/four_enu.top" topicName="four" language="en_US" nuance="enu" />
+        <Topic name="first1_enu" src="first1/first1_enu.top" topicName="first1" language="en_US" nuance="enu" />
+        <Topic name="second1_enu" src="second1/second1_enu.top" topicName="second1" language="en_US" nuance="enu" />
+        <Topic name="third1_enu" src="third1/third1_enu.top" topicName="third1" language="en_US" nuance="enu" />
+        <Topic name="four1_enu" src="four1/four1_enu.top" topicName="four1" language="en_US" nuance="enu" />
+        <Topic name="first2_enu" src="first2/first2_enu.top" topicName="first2" language="en_US" nuance="enu" />
+        <Topic name="second2_enu" src="second2/second2_enu.top" topicName="second2" language="en_US" nuance="enu" />
+        <Topic name="third2_enu" src="third2/third2_enu.top" topicName="third2" language="en_US" nuance="enu" />
+        <Topic name="four2_enu" src="four2/four2_enu.top" topicName="four2" language="en_US" nuance="enu" />
+        <Topic name="test_enu" src="test/test_enu.top" topicName="test" language="en_US" nuance="enu" />
+        <Topic name="morereco_enu" src="morereco/morereco_enu.top" topicName="morereco" language="en_US" nuance="enu" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ libFinal1/translations/translation_en_US.ts
@@ -0,0 +1,278 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>I have some suggestions for you to imrove your lexile level. You may choose The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you ready to choose now?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I have some suggestions for you to imrove your lexile level. You may choose The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you ready to choose now?</translation>
+        </message>
+        <message>
+            <source>You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (10)</name>
+        <message>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (11)</name>
+        <message>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (12)</name>
+        <message>
+            <source>You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (13)</name>
+        <message>
+            <source>There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (14)</name>
+        <message>
+            <source>You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (15)</name>
+        <message>
+            <source>I have some suggestions for you to imrove your lexile level.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I have some suggestions for you to imrove your lexile level.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (16)</name>
+        <message>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (17)</name>
+        <message>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (18)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>I have some suggestions for you to imrove your lexile level.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I have some suggestions for you to imrove your lexile level.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (5)</name>
+        <message>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (6)</name>
+        <message>
+            <source>You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (7)</name>
+        <message>
+            <source>There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (8)</name>
+        <message>
+            <source>You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (9)</name>
+        <message>
+            <source>I have some suggestions for you to imrove your lexile level.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I have some suggestions for you to imrove your lexile level.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+        <message>
+            <source>Good, below</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good, below</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+        <message>
+            <source>Good above</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good above</translation>
+        </message>
+        <message>
+            <source>Good </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+        <message>
+            <source>Good middle</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good middle</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (5)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (6)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (7)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (8)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+    </context>
+</TS>
